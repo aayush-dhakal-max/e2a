7c9ef1fb06fec170bc6f7512a61adb2ff35cd4ab
aayush dhakal <ayushdhakal84@gmail.com>
2024-05-26 12:50:18 +0545

    google maps size changed for smaller screens

--- src/components/component/Contact.tsx
+++ src/components/component/Contact.tsx
@@ -109,12 +109,12 @@ export default function Component() {
                 </div>
               </div>
             </div>
-            <div className="rounded-lg bg-white p-6 shadow-md dark:bg-gray-950">
+            <div className="rounded-lg bg-white p-3 shadow-md dark:bg-gray-950">
               <h3 className="text-2xl font-bold tracking-tighter">Find Us on Map</h3>
-              <div className="mt-4 aspect-video overflow-hidden rounded-lg">
+              <div className="mt-1 aspect-video overflow-hidden rounded-lg">
                 <iframe
                   title="Google Maps"
-                  className="rounded-sm md:h-full sm:h-full h-[300px]"
+                  className="rounded-sm h-full"
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.2645232799005!2d85.31198767525271!3d27.67821807619923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19ef1dbb9b8d%3A0x1a26f16a81a0e6e7!2sE2A%20Learning%20Nepal!5e0!3m2!1sen!2snp!4v1706521624603!5m2!1sen!2snp"
                   width="100%"
                   height="100%"
