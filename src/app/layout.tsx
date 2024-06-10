ce674efcc090f433d6c4e1492632348f55c52117
aayush dhakal <ayushdhakal84@gmail.com>
2024-06-10 12:18:48 +0545

    vercel speed analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Libre_Franklin } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const libre_franklin = Libre_Franklin({
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={libre_franklin.variable}>{children}</body>
+      <body className={libre_franklin.variable}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
