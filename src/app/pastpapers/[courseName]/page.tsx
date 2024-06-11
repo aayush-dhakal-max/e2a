fdeee58f59be844a64ad3a06962f8d3bd13a8ee4
aayush dhakal <ayushdhakal84@gmail.com>
2024-06-11 10:39:40 +0545

    computer 2023 may/june pdf files linked with google drive files

--- src/app/pastpapers/[courseName]/page.tsx
+++ src/app/pastpapers/[courseName]/page.tsx
@@ -1,44 +1,13 @@
+"use client";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
-
-const papers = [
-  {
-    subject: "Paper 1",
-    exams: [
-      { name: "Question Paper 1", href: "#" },
-      { name: "Question Paper 2", href: "#" },
-      { name: "Question Paper 3", href: "#" },
-    ],
-  },
-  {
-    subject: "Paper 2",
-    exams: [
-      { name: "Question Paper 1", href: "#" },
-      { name: "Question Paper 2", href: "#" },
-      { name: "Question Paper 3", href: "#" },
-    ],
-  },
-  {
-    subject: "Paper 3",
-    exams: [
-      { name: "Question Paper 1", href: "#" },
-      { name: "Question Paper 2", href: "#" },
-      { name: "Question Paper 3", href: "#" },
-    ],
-  },
-  {
-    subject: "Paper 4",
-    exams: [
-      { name: "Question Paper 1", href: "#" },
-      { name: "Question Paper 2", href: "#" },
-      { name: "Question Paper 3", href: "#" },
-      { name: "Question Paper 3", href: "#" },
-    ],
-  },
-];
+import { useState } from "react";
+import papers from "@/data/computerPPData.json";
 
 export default function Component() {
+  const [PdfLink, setPdfLink] = useState("");
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6 h-[100vh]">
       <div className="bg-gray-100 rounded-lg overflow-hidden">
@@ -49,8 +18,11 @@ export default function Component() {
                 <h3 className="text-lg font-medium  ">{paper.subject}</h3>
                 {paper.exams.map((exam) => (
                   <Link
+                    onClick={() => {
+                      setPdfLink(exam.href);
+                    }}
                     key={exam.name}
-                    href={exam.href}
+                    href={"#"}
                     className="block bg-gray-300 dark:bg-gray-900 rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-800 hover:text-white transition-colors"
                     prefetch={false}
                   >
@@ -65,12 +37,7 @@ export default function Component() {
       </div>
       <div className="bg-gray-100 dark:bg-gray-950 rounded-lg overflow-hidden md:block hidden">
         <div className="h-[calc(100vh-128px)] w-full">
-          <iframe
-            src={`https://drive.google.com/file/d/1gKzEZ6bqqXoggJBHGUnyy6Dka3CYkXBO/preview`}
-            frameBorder="0"
-            allowFullScreen
-            className="w-full h-full"
-          />
+          <iframe src={`${PdfLink}`} frameBorder="0" allowFullScreen className="w-full h-full" />
         </div>
       </div>
     </div>
