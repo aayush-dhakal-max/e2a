1a8b2b453fcb8b6236ffdf920f8c2518c90e0324
aayush dhakal <ayushdhakal84@gmail.com>
2024-06-12 12:07:22 +0545

    .

--- src/app/pastpapers/[courseName]/page.tsx
+++ src/app/pastpapers/[courseName]/page.tsx
@@ -6,26 +6,25 @@ import { useState } from "react";
 import papers from "@/data/computerPPData.json";
 import Sidebar from "@/components/component/Sidebar";
 import { Button } from "@/components/ui/button";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Cross1Icon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 export default function Component() {
   const [PdfLink, setPdfLink] = useState("");
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[20vw_1fr] grid-cols-1[25vw_1fr] gap-6 p-4 md:p-4 h-max">
-      {/* <div
-        className={` ${
-          isSidebarOpen ? "block" : "hidden"
-        } h-[calc(100vh-160px)] rounded-md w-96 absolute bg-red-200 md:hidden  `}
-      ></div> */}
+    <div className="grid grid-cols-1 md:grid-cols-[25vw_1fr] sm:grid-cols-1 lg:grid-cols-1[20vw_1fr] gap-6 p-4 md:p-4 h-max md:mx-12">
       <Button
         variant="ghost"
         size="icon"
         onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        className="md:hidden z-30 mb"
+        className="md:hidden z-30 mb "
       >
-        <HamburgerMenuIcon className="h-6 w-6" />
+        {isSidebarOpen ? (
+          <Cross1Icon className="block h-6 w-6" aria-hidden="true" />
+        ) : (
+          <HamburgerMenuIcon className="block h-6 w-6" aria-hidden="true" />
+        )}
         <span className="sr-only">Toggle navigation</span>
       </Button>
       <div
@@ -33,11 +32,11 @@ export default function Component() {
           isSidebarOpen ? "block pt-10 absolute h-100vh" : "hidden"
         } rounded-lg overflow-hidden md:block`}
       >
-        <ScrollArea className="md:h-[calc(100vh-160px)] h-[calc(100vh-250px)]  rounded-lg">
+        <ScrollArea className="md:h-[calc(100vh-160px)] h-[calc(100vh-250px)] md:border-none drop-shadow-lg border-black border-2 rounded-lg">
           <div className="p-4 space-y-2 bg-gray-200">
             {papers.map((paper) => (
               <div key={paper.subject}>
-                <h3 className="text-lg font-medium  ">{paper.subject}</h3>
+                <h3 className="text-lg font-medium text-[#C60B52] ">{paper.subject}</h3>
                 <div key={paper.subject} className="grid gap-2">
                   {paper.exams.map((exam) => (
                     <Link
@@ -60,7 +59,7 @@ export default function Component() {
         </ScrollArea>
       </div>
       <div className="bg-gray-100 rounded-lg overflow-hidden w-full">
-        <div className="h-[calc(100vh-160px)] w-full">
+        <div className="md:h-[calc(100vh-160px)] sm:h-[calc(100vh-100px)] h-[calc(100vh-200px)]  w-full">
           <iframe src={`${PdfLink}`} frameBorder="0" allowFullScreen className="w-full h-full" />
         </div>
       </div>
