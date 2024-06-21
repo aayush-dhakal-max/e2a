e5cb6ddb643f4ebc75cab729d8520f2b2a020ddf
aayush dhakal <ayushdhakal84@gmail.com>
2024-06-21 14:55:39 +0545

    grid cols bug fixed in pastpapers

--- src/app/pastpapers/[courseName]/page.tsx
+++ src/app/pastpapers/[courseName]/page.tsx
@@ -79,7 +79,7 @@ export default function Component() {
                                 }}
                                 key={exam.name}
                                 href={"#"}
-                                className={`block w-full bg-gray-300 rounded-md px-3 py-2 text-lg tracking-wide font-medium transition-colors duration-400 
+                                className={`block w-full bg-gray-300 rounded-md px-3 py-2 pt-3 text-lg tracking-wide font-medium transition-colors duration-400 
                               ${
                                 activeLink === exam.name
                                   ? "bg-gray-800 text-white"
@@ -128,7 +128,7 @@ export default function Component() {
                         }}
                         key={exam.name}
                         href={"#"}
-                        className={`block w-full bg-gray-300 rounded-md px-3 py-2 text-lg tracking-wide font-medium transition-colors duration-400 
+                        className={`block w-full bg-gray-300 rounded-md px-3 py-2 pt-3 text-xl tracking-wide font-medium transition-colors duration-400 
                         ${
                           activeLink === exam.name
                             ? "bg-gray-800 text-white"
